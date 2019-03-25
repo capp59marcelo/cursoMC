ba0ab327cb3d61a18aed295d21025c85d3d0f772
Marcelo Rodrigues <capp59marcelo@hotmail.com>
2019-03-25 13:00:50 -0300

    Paginação com parametros opcionas na requisição

--- src/main/java/com/marcelo/cursomc/CursomcApplication.java
+++ src/main/java/com/marcelo/cursomc/CursomcApplication.java
@@ -38,28 +38,30 @@ public class CursomcApplication implements CommandLineRunner
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	public static void main(String[] args) {
+
+	public static void main(String[] args)
+	{
 		SpringApplication.run(CursomcApplication.class, args);
 	}
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 
@@ -68,62 +70,74 @@ public class CursomcApplication implements CommandLineRunner
 	{
 		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
-		
+		Categoria categoria3 = new Categoria(null, "Cama mesa e banho");
+		Categoria categoria4 = new Categoria(null, "Eletrodomesticos");
+		Categoria categoria5 = new Categoria(null, "Eletronicos");
+		Categoria categoria6 = new Categoria(null, "Jardinagem");
+		Categoria categoria7 = new Categoria(null, "Decoração");
+		Categoria categoria8 = new Categoria(null, "Perfumaria");
+		Categoria categoria9 = new Categoria(null, "Vestuario");
+		Categoria categoria10 = new Categoria(null, "Utensílios");
+
 		Produto produto1 = new Produto(null, "Computador", 2000.00);
 		Produto produto2 = new Produto(null, "Impressora", 800.00);
 		Produto produto3 = new Produto(null, "Mouse", 80.00);
-		
+
 		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
 		categoria2.getProdutos().addAll(Arrays.asList(produto2));
-		
+
 		produto1.getCategorias().addAll(Arrays.asList(categoria1));
 		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
 		produto3.getCategorias().addAll(Arrays.asList(categoria1));
-				
-		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2)	);
-		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3)	);
-		
+
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2, categoria3, categoria4, categoria5,
+				categoria6, categoria7, categoria8, categoria9, categoria10));
+		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
+
 		Estado estado1 = new Estado(null, "Minas Gerais");
 		Estado estado2 = new Estado(null, "São Paulo");
-		
+
 		Cidade cidade1 = new Cidade(null, "Uberlândia", estado1);
 		Cidade cidade2 = new Cidade(null, "São Paulo", estado2);
 		Cidade cidade3 = new Cidade(null, "Campinas", estado2);
-		
+
 		estado1.getCidades().addAll(Arrays.asList(cidade1));
 		estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
-		
+
 		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
 		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
-		
+
 		Cliente cliente1 = new Cliente(null, "Maria Silva", "mari@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
-		
+
 		cliente1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
-		
-		Endereco endereco1 = new Endereco(null, "Rua Flores", "300", "Apt 203", "Jardim", "38220834",cliente1 ,cidade1);
-		Endereco endereco2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012",cliente1 ,cidade2);
-		
+
+		Endereco endereco1 = new Endereco(null, "Rua Flores", "300", "Apt 203", "Jardim", "38220834", cliente1,
+				cidade1);
+		Endereco endereco2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cliente1,
+				cidade2);
+
 		cliente1.getEnderecos().addAll(Arrays.asList(endereco1, endereco2));
-		
+
 		clienteRepository.saveAll(Arrays.asList(cliente1));
 		enderecoRepository.saveAll(Arrays.asList(endereco1, endereco2));
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-		
+
 		Pedido pedido1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cliente1, endereco1);
 		Pedido pedido2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cliente1, endereco2);
-		
+
 		Pagamento pagamento1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, pedido1, 6);
 		pedido1.setPagamento(pagamento1);
-		
-		Pagamento pagamento2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, pedido2, sdf.parse("20/10/2017 00:00"), null);
+
+		Pagamento pagamento2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, pedido2,
+				sdf.parse("20/10/2017 00:00"), null);
 		pedido2.setPagamento(pagamento2);
-		
+
 		cliente1.getPedidos().addAll(Arrays.asList(pedido1, pedido2));
-		
+
 		pedidoRepository.saveAll(Arrays.asList(pedido1, pedido2));
 		pagamentoRepository.saveAll(Arrays.asList(pagamento1, pagamento2));
-		
+
 		ItemPedido itemProduto1 = new ItemPedido(pedido1, produto1, 0.00, 1, 2000.00);
 		ItemPedido itemProduto2 = new ItemPedido(pedido1, produto3, 0.00, 2, 80.00);
 		ItemPedido itemProduto3 = new ItemPedido(pedido2, produto2, 100.00, 1, 800.00);
@@ -135,8 +149,8 @@ public class CursomcApplication implements CommandLineRunner
 		produto2.getItens().addAll(Arrays.asList(itemProduto3));
 		produto3.getItens().addAll(Arrays.asList(itemProduto2));
 
-		itemPedidoRepository.saveAll(Arrays.asList(itemProduto1, itemProduto2, itemProduto3));	
-		
+		itemPedidoRepository.saveAll(Arrays.asList(itemProduto1, itemProduto2, itemProduto3));
+
 	}
 
 }
--- src/main/java/com/marcelo/cursomc/services/CategoriaService.java
+++ src/main/java/com/marcelo/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marcelo.cursomc.domain.Categoria;
@@ -17,31 +20,31 @@ public class CategoriaService
 {
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public List<Categoria> findAll()
 	{
 		return repo.findAll();
 	}
-	
+
 	public Categoria find(Integer id)
 	{
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria categoria)
 	{
 		categoria.setId(null);
 		return repo.save(categoria);
 	}
-	
+
 	public Categoria update(Categoria categoria)
 	{
 		find(categoria.getId());
 		return repo.save(categoria);
 	}
-	
+
 	public void delete(Integer id)
 	{
 		find(id);
@@ -49,9 +52,15 @@ public class CategoriaService
 		{
 			repo.deleteById(id);
 		}
-		catch (DataIntegrityViolationException e) {
+		catch (DataIntegrityViolationException e)
+		{
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
-		
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction)
+	{
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
