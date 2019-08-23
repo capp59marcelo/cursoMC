edd9ff19dbe2b9e278e67c6a2eedc4aa169d8070
Marcelo Rodrigues <capp59marcelo@hotmail.com>
2019-08-23 12:15:54 -0300

    Implementado SmtpEmailService com servidor do google

--- src/main/java/com/marcelo/cursomc/config/DevConfig.java
+++ src/main/java/com/marcelo/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.marcelo.cursomc.services.DBService;
+import com.marcelo.cursomc.services.EmailService;
+import com.marcelo.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,11 @@ public class DevConfig
 		
 		dbService.instanteTesteDataBase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService()
+	{
+		return new SmtpEmailService();
 	}
 }
